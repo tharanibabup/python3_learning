40bbf184fedcba62385cd8af6af6575a7b557a21
Tharanibabu Padmanabhan <tharanibabu@gmail.com>
2020-05-14 20:31:13 +0100

    Welcome file enhanced with displaying of Python platform details

--- welcome.py
+++ welcome.py
@@ -1,1 +1,10 @@
-print("Welcome Tharani")
+#!/usr/bin/env python3
+#Copyright 2020 Tharanibabu
+
+import platform
+
+print('Welcome !')
+print('This is Python Version {}'.format(platform.python_version()))
+print('Python compiler is {}'.format(platform.python_compiler()))
+print('Python implementation is {}'.format(platform.python_implementation()))
+print('Python revision is {}'.format(platform.python_revision()))
